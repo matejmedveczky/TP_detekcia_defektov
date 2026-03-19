ecee67fb6c61589aea00854ad299acefb9a791f9
matej <matejmedveczky@gmail.com>
2026-03-19 08:36:10 +0100

    added dataset download, requirements and the .env file

---
+++ YOLO/yolo_dataset_download.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+from roboflow import Roboflow
+
+load_dotenv()
+api_key = os.getenv("ROBOFLOW_API_KEY")
+
+rf = Roboflow(api_key=api_key)
+project = rf.workspace("hemorrhagesegmentation").project("tp26_detection")
+version = project.version(1)
+dataset = version.download("yolov11")
+                
