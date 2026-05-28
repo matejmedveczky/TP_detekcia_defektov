60d7833b94bb07bbab0ec9e7111c46f1bdb3fe11
matej <matejmedveczky@gmail.com>
2026-05-28 22:42:44 +0200

    final touches

--- YOLO/yolo_dataset_download.py
+++ YOLO/yolo_dataset_download.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+from sympy import rf
 from roboflow import Roboflow
 
 load_dotenv()
@@ -7,8 +8,8 @@ api_key = os.getenv("ROBOFLOW_API_KEY")
 
 def download_dataset():
     rf = Roboflow(api_key=api_key)
-    project = rf.workspace("hemorrhagesegmentation").project("tp26_detection")
-    version = project.version(1)
+    project = rf.workspace("hemorrhagesegmentation").project("tp26_detection_v2")
+    version = project.version(2)
     dataset = version.download("yolov11")
 
     return dataset, version, project 
